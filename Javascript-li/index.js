812bc3f3c377cf634476d5c3ca7cefc0e9eba730
akibp1 <mda588825@gmail.com>
2020-04-29 22:54:45 +0600

    all done

---
+++ Javascript-li/index.js
@@ -0,0 +1,8 @@
+// good libraries
+
+//for dom manipulation = jQuery
+//for everyWhere like (mobile app) (web app) (desktop app )= lodash or underscore
+//for do math = Math.js
+//for date&time = moment js
+//only for collection = collect js
+//for rest api = axios
