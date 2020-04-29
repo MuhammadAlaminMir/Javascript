45bb34d72f206747b1a653eca4b4eacdb484cd73
alaminmir08 <63937543+alaminmir08@users.noreply.github.com>
2020-04-29 22:50:16 +0600

    dg

--- libraries/index.js
+++
@@ -1,8 +0,0 @@
-// good libraries
-
-//for dom manipulation = jQuery
-//for everyWhere like (mobile app) (web app) (desktop app )= lodash or underscore
-//for do math = Math.js
-//for date&time = moment js
-//only for collection = collect js
-//for rest api = axios
