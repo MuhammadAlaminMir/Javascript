8aac3d78b6548a9245e7d8610111c17fa9e293b2
akibp1 <mda588825@gmail.com>
2020-05-04 22:43:30 +0600

    Dom Done

---
+++ 17.Javascript-DOM/17.Must-Learn.js
@@ -0,0 +1,10 @@
+//1.Canvas
+//2.SvG
+//3.Drag and Drop
+//4.Transition
+//5.Location
+//6.Audio, Video
+//7.Speech Recognition
+//8.Web Socket
+//9.Local Storage, Cookies and Browser DataBases
+//10.
