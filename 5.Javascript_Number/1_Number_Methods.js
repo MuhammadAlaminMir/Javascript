0adf8940e193b5aec566ada9be6a96630545693c
MuhammadAlaminMir <alaminmir08@gmail.com>
2025-10-26 17:24:41 +0600

    Learned more about number methods and property's

--- 5.Javascript_Number/1_Number_Methods.js
+++ 5.Javascript_Number/1_Number_Methods.js
@@ -13,3 +13,16 @@ console.log((8).toString(2)); // "1000" (binary)
 
 // toExponential(): Returns a string in exponential notation
 console.log(num.toExponential()); // "1.23456e+2"
+
+// Converting variables to Numbers
+
+console.log(Number("123.45")); // 123.45
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+console.log(Number("123abc")); // NaN (not a number)    
+console.log(Number(null)); // 0
+console.log(Number(undefined)); // NaN  
+console.log(Number("   123   ")); // 123 (ignores whitespace)
+
+console.log(parseInt("123abc")); // 123 (ignores non-numeric characters)
+console.log(parseFloat("123.45abc")); // 123.45 (ignores non-numeric characters)
---
+++ 5.Javascript_Number/3_Number_Property.js
@@ -0,0 +1,5 @@
+console.log(Number.MAX_VALUE); // Largest possible number
+console.log(Number.MIN_VALUE); // Smallest positive number
+console.log(Number.POSITIVE_INFINITY); // Infinity
+console.log(Number.NEGATIVE_INFINITY); // -Infinity
+console.log(Number.NaN); // NaN
